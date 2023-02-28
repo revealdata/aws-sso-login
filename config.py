d6a914b4ff3f0626e49ce2f301effc0fb8bb783b
Russ Cook <rcook@revealdata.com>
2023-02-28 12:13:00 -0600

    version 1.2.0

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from lib.classes import Argument
 APP = {
     'name': 'aws-sso-login',
     'description': 'AWS SSO Login Manager',
-    'version': '1.1.0',
+    'version': '1.2.0',
     'author': 'Russ Cook <rcook@revealdata.com>',
     'url': 'https://github.com/revealdata/aws-sso-login'
 }
